e077cd94b615885a5b9640f460492e0c2a8754e5
Noobcoder-xD <157066626+Noobcoder-xD@users.noreply.github.com>
2024-02-20 18:11:44 +0530

    Update

---
+++ constants.py
@@ -0,0 +1,6 @@
+
+from os import environ
+from constants import API_ID
+from logging import basicConfig, INFO, StreamHandler, getLogger, WARNING, Logger
+from logging.handlers import RotatingFileHandler
+from script import StartTxT, HelpTxT, AboutTxT
